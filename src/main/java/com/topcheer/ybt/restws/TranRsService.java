08f8e81761eb52c8ed96df7df7ef19d511f76ddf
zhouxuan <352400260@qq.com>
2016-09-25 11:29:58 +0800

    rest测试提交

--- src/main/java/com/topcheer/ybt/restws/TranRsService.java
+++ src/main/java/com/topcheer/ybt/restws/TranRsService.java
@@ -3,10 +3,14 @@ package com.topcheer.ybt.restws;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -17,6 +21,7 @@ import com.google.common.collect.Lists;
 import com.topcheer.ybt.data.InsPrdForGridResult;
 import com.topcheer.ybt.data.InsPrdResult;
 import com.topcheer.ybt.data.MenuResult;
+import com.topcheer.ybt.system.entity.TopMenuinfo;
 import com.topcheer.ybt.system.service.ITopMenuinfoService;
 
 /**
@@ -32,9 +37,9 @@ public class TranRsService {
 	ITopMenuinfoService menuService;
 
 	@GET
-	@Path("getInsPrdInfos/{bankCode}")
+	@Path("/getInsPrdInfos/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<InsPrdForGridResult> searchPrds(@PathParam("bankCode") String bankCode) {
+	public List<InsPrdForGridResult> searchPrds(@FormParam("bankCode") String bankCode) {
 		if (Strings.isNullOrEmpty(bankCode)) {
 			String msg = "机构号为空";
 			throw buildException(Status.NOT_FOUND, msg);
@@ -46,14 +51,15 @@ public class TranRsService {
 
 	}
 
-	@GET
-	@Path("findInsPrdInfoByCode/{insPrdCode}")
+	@Path("findInsPrdInfoByCode")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public InsPrdResult findSpecialPrd(@PathParam("insPrdCode") String insPrdCode) {
-		if (Strings.isNullOrEmpty(insPrdCode)) {
+	public InsPrdResult findSpecialPrd(@QueryParam("topMenuinfo") String topMenuinfo) {
+		/*if (Strings.isNullOrEmpty(topMenuinfo)) {
 			String msg = "产品编号为空";
 			throw buildException(Status.NOT_FOUND, msg);
-		}
+		}*/
 		return new InsPrdResult("100001", "28", "1", "60", "2", "每天不到3毛钱，即可享受最高100万的保障", "保险责任详细信息请参考以下相关文档");
 	}
 
