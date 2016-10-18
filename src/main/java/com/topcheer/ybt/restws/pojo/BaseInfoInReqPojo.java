6938590e72f67ab127ba94710fec220d46784f62
xuxl001 <420643134@qq.com>
2016-10-18 17:16:02 +0800

    更改接口文件名

---
+++ src/main/java/com/topcheer/ybt/restws/pojo/BaseInfoInReqPojo.java
@@ -0,0 +1,60 @@
+package com.topcheer.ybt.restws.pojo;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class BaseInfoInReqPojo {
+
+	private String insCorpCode; //保险公司编号
+	private String bankCode; //开户卡所属支行
+	private String insPrdCode;//保险产品编码
+	private String channelType;//渠道标志
+	
+	
+	
+	public BaseInfoInReqPojo() {
+		super();
+	}
+	public BaseInfoInReqPojo(String insCorpCode, String bankCode,
+			String insPrdCode, String channelType) {
+		super();
+		this.insCorpCode = insCorpCode;
+		this.bankCode = bankCode;
+		this.insPrdCode = insPrdCode;
+		this.channelType = channelType;
+	}
+	public String getInsCorpCode() {
+		return insCorpCode;
+	}
+	public void setInsCorpCode(String insCorpCode) {
+		this.insCorpCode = insCorpCode;
+	}
+	public String getBankCode() {
+		return bankCode;
+	}
+	public void setBankCode(String bankCode) {
+		this.bankCode = bankCode;
+	}
+	public String getInsPrdCode() {
+		return insPrdCode;
+	}
+	public void setInsPrdCode(String insPrdCode) {
+		this.insPrdCode = insPrdCode;
+	}
+	public String getChannelType() {
+		return channelType;
+	}
+	public void setChannelType(String channelType) {
+		this.channelType = channelType;
+	}
+	@Override
+	public String toString() {
+		return "BaseInfoInPojo [insCorpCode=" + insCorpCode + ", bankCode="
+				+ bankCode + ", insPrdCode=" + insPrdCode + ", channelType="
+				+ channelType + "]";
+	}
+	
+	
+}
